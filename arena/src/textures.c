b5992b0eb594a27806926444718fcf503e437ba5
Bendi11 <bkliebmann@gmail.com>
2024-04-06 19:54:16 -0700

    Renamed embedded image files to satisfy case-insensitive path handling

--- arena/src/textures.c
+++ arena/src/textures.c
@@ -5,29 +5,27 @@
 #include <SDL2/SDL_image.h>
 #endif
 
-#include "img/k.h"
-#include "img/q.h"
-#include "img/r.h"
-#include "img/b.h"
-#include "img/n.h"
-#include "img/p.h"
+#include "img/wk.h"
+#include "img/wq.h"
+#include "img/wr.h"
+#include "img/wb.h"
+#include "img/wn.h"
+#include "img/wp.h"
 
 
-#include "img/K.h"
-#include "img/Q.h"
-#include "img/R.h"
-#include "img/B.h"
-#include "img/N.h"
-#include "img/P.h"
+#include "img/bk.h"
+#include "img/bq.h"
+#include "img/br.h"
+#include "img/bb.h"
+#include "img/bn.h"
+#include "img/bp.h"
 
 #include "img/marble.h"
 #include "img/wood.h"
 
-#include "max/piece.h"
 #include <SDL_render.h>
 #include <SDL_rwops.h>
 #include <SDL_surface.h>
-#include <stdint.h>
 
 static SDL_Texture* load_texture(SDL_Renderer *render, uint8_t *buf, size_t len) {
     SDL_Texture *tx = NULL;
@@ -51,19 +49,19 @@ static SDL_Texture* load_texture(SDL_Renderer *render, uint8_t *buf, size_t len)
 }
 
 int gui_textures_load(SDL_Renderer *render, gui_textures_t *textures) {
-    textures->white[MAX_PIECECODE_PAWN]   = load_texture(render, __p_png, __p_png_len);
-    textures->white[MAX_PIECECODE_KNIGHT] = load_texture(render, __n_png, __n_png_len);
-    textures->white[MAX_PIECECODE_BISHOP] = load_texture(render, __b_png, __b_png_len);
-    textures->white[MAX_PIECECODE_ROOK]   = load_texture(render, __r_png, __r_png_len);
-    textures->white[MAX_PIECECODE_QUEEN]  = load_texture(render, __q_png, __q_png_len);
-    textures->white[MAX_PIECECODE_KING]   = load_texture(render, __k_png, __k_png_len);
+    textures->white[MAX_PIECECODE_PAWN]   = load_texture(render, __wp_png, __wp_png_len);
+    textures->white[MAX_PIECECODE_KNIGHT] = load_texture(render, __wn_png, __wn_png_len);
+    textures->white[MAX_PIECECODE_BISHOP] = load_texture(render, __wb_png, __wb_png_len);
+    textures->white[MAX_PIECECODE_ROOK]   = load_texture(render, __wr_png, __wr_png_len);
+    textures->white[MAX_PIECECODE_QUEEN]  = load_texture(render, __wq_png, __wq_png_len);
+    textures->white[MAX_PIECECODE_KING]   = load_texture(render, __wk_png, __wk_png_len);
 
-    textures->black[MAX_PIECECODE_PAWN]   = load_texture(render, __P_png, __P_png_len);
-    textures->black[MAX_PIECECODE_KNIGHT] = load_texture(render, __N_png, __N_png_len);
-    textures->black[MAX_PIECECODE_BISHOP] = load_texture(render, __B_png, __B_png_len);
-    textures->black[MAX_PIECECODE_ROOK]   = load_texture(render, __R_png, __R_png_len);
-    textures->black[MAX_PIECECODE_QUEEN]  = load_texture(render, __Q_png, __Q_png_len);
-    textures->black[MAX_PIECECODE_KING]   = load_texture(render, __K_png, __K_png_len);
+    textures->black[MAX_PIECECODE_PAWN]   = load_texture(render, __bp_png, __bp_png_len);
+    textures->black[MAX_PIECECODE_KNIGHT] = load_texture(render, __bn_png, __bn_png_len);
+    textures->black[MAX_PIECECODE_BISHOP] = load_texture(render, __bb_png, __bb_png_len);
+    textures->black[MAX_PIECECODE_ROOK]   = load_texture(render, __br_png, __br_png_len);
+    textures->black[MAX_PIECECODE_QUEEN]  = load_texture(render, __bq_png, __bq_png_len);
+    textures->black[MAX_PIECECODE_KING]   = load_texture(render, __bk_png, __bk_png_len);
 
     textures->square[0]                   = load_texture(render, __marble_png, __marble_png_len);
     textures->square[1]                   = load_texture(render, __wood_png,   __wood_png_len);
