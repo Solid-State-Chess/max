c104e081a949f5e917fe333451836c2e7269cf50
Bendi11 <bkliebmann@gmail.com>
2024-04-08 17:51:11 -0700

    Re-flip view axis in gui

--- arena/src/gui.c
+++ arena/src/gui.c
@@ -67,7 +67,7 @@ static max_bpos_t screen_to_board(gui_state_t *state, int x, int y) {
     int x_step = w / 8;
     int y_step = h / 8;
 
-    return max_bpos_new(x / x_step, y / y_step);
+    return max_bpos_new(x / x_step, 7 - (y / y_step));
 }
 
 static SDL_Texture* gui_texture_for_piece(gui_state_t *state, max_piececode_t piece) {
@@ -89,7 +89,7 @@ static void gui_state_render(gui_state_t *state) {
     for(unsigned x = 0; x < 8; ++x) {
         for(unsigned y = 0; y < 8; ++y) {
             dest.x = x * state->squarex;
-            dest.y = y * state->squarey;
+            dest.y = (7 - y) * state->squarey;
 
             max_bpos_t pos = max_bpos_new(x, y);
             SDL_Texture *bg = NULL;
