f75886a1ece45a923cd5223e4ab44baac41935f4
Bendi11 <bkliebmann@gmail.com>
2024-05-12 20:08:13 -0700

    Added additional discovered check unit test

--- include/max/engine/tt.h
+++ include/max/engine/tt.h
@@ -1,4 +1,6 @@
+/// \file tt.h
 #pragma once
+#include <stdint.h>
 
 /// \ingroup engine
 /// @{
@@ -9,13 +11,20 @@
 /// speeding up game tree searches by eliminating already-searched portions of the tree.
 /// @{
 
+
+#ifdef MAX_ZOBRIST_64
+typedef uint32_t max_ttentry_key_t;
+#else
+typedef uint16_t max_ttentry_key_t;
+#endif
+
 /// An entry in the transposition table containing the result of a prior evaluation.
 /// The size of this entry has massive effects on the memory footprint of the program as the transposition table
 /// should be the largest object in the engine, so reducing it's size is extremely important.
 /// It represents a best or refutation move in as compact a form as possible to reflect this, and packs all attributes into
 /// as few bits as possible.
 typedef struct {
-
+    
 } max_ttentry_t;
 
 /// @}
--- src/board/test/check.c
+++ src/board/test/check.c
@@ -1,8 +1,33 @@
 #include "max/board/board.h"
+#include "max/board/fen.h"
+#include "max/board/move.h"
+#include "max/board/movegen.h"
+#include "max/board/squares.h"
+#include "max/board/state.h"
+#include "private/board/board.h"
+#include "private/test.h"
+
+#ifdef MAX_TESTS
 
 void max_board_check_unit_tests(void) {
     max_state_t buf[8];
 
     max_board_t board;
     max_board_new(&board, buf, MAX_ZOBRIST_DEFAULT_SEED);
+    
+    max_fen_parse_err_t ec;
+    ASSERT(
+        (ec = max_board_parse_from_fen(&board, "rnbq1bnr/pp1pkppp/8/1Pp1p3/8/B7/P1PPPPPP/RN1QKBNR w KQ-- c6 0 1")) == MAX_FEN_SUCCESS,
+        "Failed to parse FEN string: %s",
+        max_fen_parse_err_str(ec)
+    );
+
+    max_board_make_move(&board, max_smove_new(MAX_B5, MAX_C6, MAX_MOVETAG_ENPASSANT));
+    ASSERT(
+        !max_check_is_empty(max_board_state(&board)->check[0]) &&
+        max_check_is_sliding(max_board_state(&board)->check[0]),
+        "Check is not detected when discovered via en passant"
+    );
 }
+
+#endif
